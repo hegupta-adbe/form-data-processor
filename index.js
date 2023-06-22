347f834bf2128cd09d7e56229cb80a4e3ed0f5b9
hegupta-adbe <124023648+hegupta-adbe@users.noreply.github.com>
2023-06-22 17:14:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ try {
   const clientId = core.getInput('azure-client-id');
   const clientSecret = core.getInput('azure-client-secret');
   const resource = core.getInput('azure-resource');
-  const payload = JSON.stringify(github.context.payload.client_payload, undefined, 2);
-  const result = `Received client ID ${clientId}, client secret ${clientSecret}, app URI ${resource}, event payload ${payload}`
-  console.log(result)
-  core.setOutput("processing-result", result);
+  const payload = github.context.payload.client_payload
+  const resp = {clientId, clientSecret, resource, payload}
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(resp),
+    headers: { 'Content-Type': 'application/json' }
+  }
+  const response = await fetch('https://echo.zuplo.io', options)
+  const jsonResponse = await response.json();
+  console.log(jsonResponse)
+  core.setOutput("processing-result", jsonResponse);
 } catch (error) {
   core.setFailed(error.message);
 }
